aee150f9cefff662f448851b68657d6b708c2246
patrick_welter <pa.welter@web.de>
2021-10-21 16:37:33 +0200

    implement simple generator function #5

--- python/test-data-generator/configuration.py
+++ python/test-data-generator/configuration.py
@@ -1,13 +1,17 @@
 import json
 
-FILE_PATH = "config.json"
+FILE_PATH = "test-data-generator/config.json"
 
 class Configuration:
 
     seed: int
+    image_size: int
+    image_amount: int
 
-    def __init__(self, seed) -> None:
+    def __init__(self, seed, imageSize, imageAmount) -> None:
         self.seed = seed
+        self.image_size = imageSize
+        self.image_amount = imageAmount
 
 def load() -> Configuration:
     with open(FILE_PATH) as config_file:
--- python/test-data-generator/generator.py
+++ python/test-data-generator/generator.py
@@ -1,3 +1,25 @@
 import configuration as config
+import os, shutil
+from PIL import Image, ImageDraw 
 
-CONFIG: config.Configuration = config.load()
+#todo: clear export dic before saving
+
+class Generator:
+    CONFIG = config.load()
+    EXPORT_DIR = "data/training"
+
+    def cleanExportDir(self) -> None:
+        shutil.rmtree(self.EXPORT_DIR)
+        os.mkdir(self.EXPORT_DIR)
+
+    def generate(self) -> None:
+        self.cleanExportDir()
+        
+        digit = len(str(self.CONFIG.image_amount - 1))
+        for i in range(0, self.CONFIG.image_amount):
+            image = Image.new("L", (self.CONFIG.image_size, self.CONFIG.image_size), 255)
+            draw = ImageDraw.Draw(image)
+            draw.ellipse([(0, 0), (27, 27)], fill="black")
+            image.save(f"{self.EXPORT_DIR}/sample_circle_{i:0{digit}d}.png", "PNG")
+
+Generator().generate()
