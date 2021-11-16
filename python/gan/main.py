7282c65b082cc958de2ccb5305da4cb1a660f33c
Jonas Buergel <burgeljonas@gmail.com>
2021-11-16 17:04:17 +0100

    adjust training #16

--- python/gan/main.py
+++ python/gan/main.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, Sequential, Input, optimizers, losses, preprocessing, models
 import os
 from tqdm import tqdm
+import numpy as np
 
 
 # largely inspired by these tutorials:
@@ -14,6 +15,7 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 physical_devices = tf.config.list_physical_devices("GPU")
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
+EPOCHS = 10000
 BATCH_SIZE = 28
 LABEL_AMOUNT = 2
 LATENT_DIM = 100
@@ -128,10 +130,57 @@ def define_gan(generator: models.Model, discriminator: models.Model) -> models.M
     return model
 
 
+def random_generator_input(batch_size: int, latent_dim: int, label_amount: int):
+    noise = np.random.randn(
+        latent_dim * batch_size).reshape(batch_size, latent_dim)
+    labels = np.random.randint(0, label_amount, batch_size)
+    return [noise, labels]
+
+
+def generate_fake_data(generator: models.Model, batch_size: int, latent_dim: int, label_amount: int):
+    noise, labels = random_generator_input(
+        batch_size, latent_dim, label_amount)
+    return [generator.predict([noise, labels]), labels]
+
+
+def train(dataset: tf.data.Dataset, discriminator: models.Model, generator: models.Model, gan: models.Model, epochs: int, latent_dim: int, label_amount: int):
+    for epoch in tqdm(range(epochs)):
+        for batch in dataset:
+            # cannot be set statically -> e.g. if dataset is not dividable by batch-size the last batch is smaller
+            batch_size = batch[0].shape[0]
+
+            # train discriminator
+            discriminator.train_on_batch(
+                batch,
+                tf.ones((batch_size, 1))
+            )
+            fake_data = generate_fake_data(
+                generator, batch_size, latent_dim, label_amount)
+            discriminator.train_on_batch(
+                fake_data,
+                tf.zeros((batch_size, 1))
+            )
+
+            # train generator through gan
+            gan.train_on_batch(
+                random_generator_input(batch_size, latent_dim, label_amount),
+                tf.ones((batch_size, 1))
+            )
+
+            # save some samples
+            if epoch % 100 == 0:
+                images, labels = fake_data
+                img = preprocessing.image.array_to_img(images[0])
+                img.save(
+                    f"data/results/epoch_{epoch}_label_{labels[0]}.png")
+
+
 discriminator = define_discriminator(IMG_SIZE, IMG_CHANNELS, LABEL_AMOUNT)
 generator = define_generator(LATENT_DIM, LABEL_AMOUNT)
 gan = define_gan(generator, discriminator)
 print(gan.summary())
+
+train(dataset, discriminator, generator, gan, EPOCHS, LATENT_DIM, LABEL_AMOUNT)
 exit(0)
 
 discriminator = Sequential(
