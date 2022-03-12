74438e2a6492207d04857834e9dbce5ea7ac5ff8
Jonas Buergel <burgeljonas@gmail.com>
2022-03-12 15:44:52 +0100

    add smoothness to discriminator

--- python/code/training/loss.py
+++ python/code/training/loss.py
@@ -5,9 +5,10 @@ import sys
 sys.path.append(".")
 # This method returns a helper function to compute cross entropy loss
 from constants import LOSS_FUNCTION as cross_entropy
+from constants import SMOOTH
 
 def discriminator_loss(real_output, fake_output):
-    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    real_loss = cross_entropy(tf.ones_like(real_output) * (1 - SMOOTH), real_output)
     fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
     total_loss = real_loss + fake_loss
     return total_loss
