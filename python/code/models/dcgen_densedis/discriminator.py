b64fdf52b3112b91a7631a09f373b3d1423b45c3
Jonas Buergel <jonas.buergel@cas.de>
2022-04-07 17:19:53 +0200

    remove border around architecture images

--- python/code/models/dcgen_densedis/discriminator.py
+++ python/code/models/dcgen_densedis/discriminator.py
@@ -1,54 +1,39 @@
-from keras import Sequential
 from keras.layers import *
 from keras.models import Model
-from keras.initializers.initializers_v2 import GlorotNormal
 import sys
 
 # Adds higher directory to python modules path.
 sys.path.append(".")
 
 def make_discriminator_model() -> Model:
-    from constants import NUM_CLASSES, IMG_DIM, IMG_SIZE
-    initializer = GlorotNormal()
+    from constants import NUM_CLASSES, IMG_DIM, IMG_SIZE, DROPOUT
 
     input_label = Input(shape=(1,), dtype='int32')
-    in_label = Embedding(NUM_CLASSES, IMG_DIM, embeddings_initializer=initializer)(input_label)
+    in_label = Embedding(NUM_CLASSES, IMG_DIM)(input_label)
     in_label = Flatten()(in_label)
 
     input_img = Input(shape=(IMG_SIZE, IMG_SIZE))
     in_img = Flatten()(input_img)
 
-    input_discriminator = multiply([in_img, in_label])
+    merge = multiply([in_img, in_label])
 
-    validity = discriminator()(input_discriminator)
+    merge = Dense(128, input_shape=(IMG_DIM,))(merge)
+    merge = LeakyReLU(alpha=0.2)(merge)
 
-    model = Model([input_img, input_label], validity, name="Discriminator_Input")
+    # hidden layer 1
+    merge = Dense(256)(merge)
+    merge = LeakyReLU(alpha=0.2)(merge)
 
-    return model
+    # hidden layer 2
+    merge = Dense(512)(merge)
+    merge = LeakyReLU(alpha=0.2)(merge)
 
+    merge = Flatten()(merge)
+    merge = Dropout(DROPOUT)(merge)
 
-def discriminator() -> Model:
-    from constants import IMG_DIM, DROPOUT, EMBEDDING_SIZE
-    initializer = GlorotNormal()
+    # output
+    merge = Dense(1)(merge)
 
-    model = Sequential([
-        # input
-        Dense(128, input_shape=(IMG_DIM,), kernel_initializer=initializer, name="input"),
-        LeakyReLU(alpha=0.2),
-
-        # hidden layer 1
-        Dense(256, name="hidden1", kernel_initializer=initializer),
-        LeakyReLU(alpha=0.2),
-
-        # hidden layer 2
-        Dense(512, name="hidden2", kernel_initializer=initializer),
-        LeakyReLU(alpha=0.2),
-
-        Flatten(),
-        Dropout(DROPOUT),
-
-        # output
-        Dense(1, name="output", kernel_initializer=initializer),
-    ], name="Discriminator_Layers")
+    model = Model([input_img, input_label], merge, name="Discriminator_Input")
 
     return model
--- python/code/models/densegen_dcdis/generator.py
+++ python/code/models/densegen_dcdis/generator.py
@@ -1,4 +1,4 @@
-from keras import Sequential, layers, models
+from keras import models
 from keras.layers import *
 from keras.models import Model
 from keras.initializers.initializers_v2 import RandomNormal
@@ -8,43 +8,31 @@ import sys
 sys.path.append(".")
 
 def make_generator_model() -> Model:
-    from constants import NUM_CLASSES, LATENT_DIM
+    from constants import NUM_CLASSES, LATENT_DIM, IMG_DIM, LATENT_DIM, IMG_SIZE
 
-    input_label = layers.Input(shape=(1,), dtype='int32')
-    in_label = layers.Embedding(NUM_CLASSES, LATENT_DIM)(input_label)
-    in_label = layers.Flatten()(in_label)
+    input_label = Input(shape=(1,), dtype='int32')
+    in_label = Embedding(NUM_CLASSES, LATENT_DIM)(input_label)
+    in_label = Flatten()(in_label)
 
-    input_latent = layers.Input(shape=(LATENT_DIM,))
+    input_latent = Input(shape=(LATENT_DIM,))
 
-    input_generator = layers.multiply([input_latent, in_label])
+    merge = multiply([input_latent, in_label])
 
-    image = generator()(input_generator)
+    merge = Dense(128, input_shape=(LATENT_DIM,), kernel_initializer=RandomNormal(stddev=0.02))(merge)
+    LeakyReLU(alpha=0.2)
+    BatchNormalization(momentum=0.8)
+
+    merge = Dense(256)(merge)
+    merge = LeakyReLU(alpha=0.2)(merge)
+    merge = BatchNormalization(momentum=0.8)(merge)
+
+    merge = Dense(256)(merge)
+    merge = LeakyReLU(alpha=0.2)(merge)
+    merge = BatchNormalization(momentum=0.8)(merge)
+
+    merge = Dense(IMG_DIM, activation='tanh')(merge)
+    merge = Reshape((IMG_SIZE, IMG_SIZE))(merge)
     
-    model = models.Model([input_latent, input_label], image, name="Generator_Inputs")
-
-    return model
-
-def generator() -> Model:
-    from constants import IMG_DIM, LATENT_DIM, IMG_SIZE
-
-    model = Sequential([
-        # input
-        Dense(128, input_shape=(LATENT_DIM,), kernel_initializer=RandomNormal(stddev=0.02), name="input"),
-        LeakyReLU(alpha=0.2),
-        BatchNormalization(momentum=0.8),
-
-        # hidden layer 1
-        Dense(256, name="hidden1"),
-        LeakyReLU(alpha=0.2),
-        BatchNormalization(momentum=0.8),
-
-        # hidden layer 2
-        Dense(256, name="hidden2"),
-        LeakyReLU(alpha=0.2),
-        BatchNormalization(momentum=0.8),
-
-        Dense(IMG_DIM, activation='tanh', name="output"),
-        Reshape((IMG_SIZE, IMG_SIZE)),
-    ], name="Generator_Layers")
+    model = models.Model([input_latent, input_label], merge, name="Generator_Inputs")
 
     return model
